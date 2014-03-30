dfff493fa65cb15b30b9999b40545c0d76c9604a
Santi Muñoz <santi.munozm@gmail.com>
2014-03-30 21:14:48 +0200

    Corregit el javadoc, el format correcte es @return user AQUI EL COMMENT. El format incorrecte es: @return user: AQUI EL COMMENT. Els dos punts no s'han de possar

--- src/main/java/com/tripbox/api/interfaces/GroupREST.java
+++ src/main/java/com/tripbox/api/interfaces/GroupREST.java
@@ -8,21 +8,21 @@ public interface GroupREST {
 	
 	/**
 	 * Funcio de la API que retorna un grup a partir de la seva ID.
-	 * @param id: Identificador del grup que s'ha de retornar
-	 * @return: Retorna el grup amb l'identificador d'entrada
+	 * @param id Identificador del grup que s'ha de retornar
+	 * @return Retorna el grup amb l'identificador d'entrada
 	 */
 	public Response getGroup(String id);
 	
 	/**
 	 * Funcio de la API que modifica un grup a la BD o li assigna una ID i l'inserta si es nou.
-	 * @param group: Objecte grup que ha de ser inserit o modificat a la BD.
-	 * @return: Retorna el grup modificat, amb la nova ID si correspont, i una resposta 200 si s'ha fet l'operacio correctament.
+	 * @param group Objecte grup que ha de ser inserit o modificat a la BD.
+	 * @return Retorna el grup modificat, amb la nova ID si correspont, i una resposta 200 si s'ha fet l'operacio correctament.
 	 */
 	public Response putGroup(Group group);
 	
 	/**
 	 * Funcio de la API que esborra un grup a partir de la seva ID.
-	 * @param id: Identificador del grup que s'ha d'esborrar
+	 * @param id Identificador del grup que s'ha d'esborrar
 	 */
 	public Response deleteGroup(String id);
 
--- src/main/java/com/tripbox/bbdd/interfaces/Querys.java
+++ src/main/java/com/tripbox/bbdd/interfaces/Querys.java
@@ -7,15 +7,15 @@ public interface Querys {
 	
 	/**
 	 * Funcio de l'objecte Mock que retorna un usuari a partir de la seva ID.
-	 * @param id: Identificador de l'usuari que s'ha de retornar
-	 * @return: Retorna l'usuari amb l'identificador d'entrada
+	 * @param id Identificador de l'usuari que s'ha de retornar
+	 * @return Retorna l'usuari amb l'identificador d'entrada
 	 * @throws Exception: En cas que no trobi cap usuari amb la ID indicada.
 	 */
 	public User getUser(String id) throws Exception;
 	
 	/**
 	 * Funcio de l'objecte Mock que introdueix o sobreescriu un usuari.
-	 * @param user: Usuari que ha de ser introduit a la BD.
+	 * @param user Usuari que ha de ser introduit a la BD.
 	 * @throws Exception
 	 */
 	public void putUser(User user) throws Exception;
@@ -23,15 +23,15 @@ public interface Querys {
 	
 	/**
 	 * Funcio de l'objecte Mock que retorna un grup a partir de la seva ID.
-	 * @param id: Identificador del grup que s'ha de retornar
-	 * @return: Retorna el grup amb l'identificador d'entrada
+	 * @param id Identificador del grup que s'ha de retornar
+	 * @return Retorna el grup amb l'identificador d'entrada
 	 * @throws Exception: En cas que no trobi cap grup amb la ID indicada.
 	 */
 	public Group getGroup(String id) throws Exception;
 	
 	/**
 	 * Funcio de l'objecte Mock que introdueix o sobreescriu un grup.
-	 * @param group: Grup que ha de ser introduit a la BD.
+	 * @param group Grup que ha de ser introduit a la BD.
 	 * @throws Exception
 	 */
 	public void putGroup(Group group) throws Exception;
