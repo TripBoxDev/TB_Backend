3bf8eb9d34763db45cbca9255a701d5e9bb5ac28
Cristian <krystyan_fs@hotmail.com>
2014-04-02 16:13:03 +0200

    MethodNotImplementedException implementado. (No testeado)

---
+++ src/main/java/com/tripbox/api/exceptions/MethodNotImplementedException.java
@@ -0,0 +1,24 @@
+package com.tripbox.api.exceptions;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+public class MethodNotImplementedException extends WebApplicationException {
+
+	/**
+	 * Create a HTTP 501 (Not Implemented) exception.
+	 */
+	public MethodNotImplementedException() {
+		super(Response.status(Response.Status.NOT_IMPLEMENTED).build());
+	}
+
+	/**
+	 * Create a HTTP 501 (Not Implemented) exception.
+	 * 
+	 * @param message the String that is the entity of the 501 response.
+	 */
+	public MethodNotImplementedException(String message) {
+		super(Response.status(Response.Status.NOT_IMPLEMENTED).entity(message)
+				.type("text/plain").build());
+	}
+}
