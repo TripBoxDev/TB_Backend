f254b376cf8e3799dc90d17e0430660d0b17a087
Cristian <krystyan_fs@hotmail.com>
2014-05-11 22:51:20 +0200

    arreglo

--- src/main/java/com/tripbox/elements/Group.java
+++ src/main/java/com/tripbox/elements/Group.java
@@ -47,7 +47,6 @@ public class Group {
 		this.transportCards = transportCards;
 		this.placeToSleepCards = placeToSleepCards;
 		this.otherCards = otherCards;
-		this.flagImage = false;
 	}
 
 	public String getId() {
